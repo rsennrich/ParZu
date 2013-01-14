0f4d7270656cd54b5264bfdea74c78480b6ab428
Rico Sennrich <rico.sennrich@gmx.ch>
2013-01-14 19:35:29 +0100

    remove one of the hacks for SMOR preprocessing (last commit solved the issue mostly; requiring matching bigram no longer necessary and causes other problems)

--- preprocessor/morphology/morphisto2prolog.py
+++ preprocessor/morphology/morphisto2prolog.py
@@ -298,7 +298,7 @@ def LCS(X, Y):
     C = [[0] * (n+1) for i in range(m+1)]
     for i in range(1, m+1):
         for j in range(1, n+1):
-            if X[i-1] == Y[j-1] and X[i-2] == Y[j-2]: 
+            if X[i-1] == Y[j-1]:
                 C[i][j] = C[i-1][j-1] + 1
             else:
                 C[i][j] = max(C[i][j-1], C[i-1][j])
