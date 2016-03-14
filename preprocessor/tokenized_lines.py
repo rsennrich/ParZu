aa72d50439948021d3cf5990dd6a530635052b5e
Rico Sennrich <rico.sennrich@gmx.ch>
2016-03-14 18:52:06 +0000

    more robust handling of unicode whitespace

--- preprocessor/tokenized_lines.py
+++ preprocessor/tokenized_lines.py
@@ -5,12 +5,19 @@
 # input: tokenized text, one sentence per line, whitespace to indicate token boundary
 # output: tokenized text, one token per line, empty lines to indicate sentence boundary
 
-from __future__ import print_function
+from __future__ import print_function, unicode_literals
 
 import sys
+import codecs
+
+# python 2/3 compatibility
+if sys.version_info < (3, 0):
+  sys.stderr = codecs.getwriter('UTF-8')(sys.stderr)
+  sys.stdout = codecs.getwriter('UTF-8')(sys.stdout)
+  sys.stdin = codecs.getreader('UTF-8')(sys.stdin)
 
 for line in sys.stdin:
-       line = line.replace(' \xe2\x80\x8b ', ' \xc2\xad ') #zero-width space behaves weirdly in some python versions
+       line = line.replace(' \u200b ', ' \xad ') #zero-width space behaves weirdly in some python versions
        for word in line.split():
             print(word)
        print('')
